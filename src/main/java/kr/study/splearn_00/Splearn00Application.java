38cd59862efe0d31782956598639fd51a37ad915
곽기진 <upper0house@gmail.com>
2025-08-07 19:27:16 +0900

    feat: init 초기 데이터

---
+++ src/main/java/kr/study/splearn_00/Splearn00Application.java
@@ -0,0 +1,13 @@
+package kr.study.splearn_00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Splearn00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Splearn00Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/study/splearn_00/Splearn00ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.study.splearn_00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Splearn00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
